bb90bc2004538707b7c1d2e8c2e0200cfed90447
Max Peixoto <maxmdps@gmail.com>
2012-05-17 19:41:30 -0300

    Coloquei o ActiveAdmin

--- agenda/config/routes.rb
+++ agenda/config/routes.rb
@@ -1,4 +1,8 @@
 Agenda::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :contatos
 
   # The priority is based upon order of creation:
