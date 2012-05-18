a2b98101a99ee0295b461a5ecb9612f9a4d6e54c
Franzé Jr <franzejr@gmail.com>
2012-05-17 21:16:41 -0300

    Tentativa de usar a gem El_finder

---
+++ fileManager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FileManager::Application.config.secret_token = '2ef2d775f9b98c5a4f44b9465d84ae7cec88ed6c991eb1f9fb85916d59629f205a7633d54ee33382501f9081e1b17d38b96dc2b3aa1ac61b06472a05faf9c504'
